d85c0c7cba954b1ff7d83e85e096d5a6408be53d
Shahid1530 <shaikshoaibofficialmail@gmail.com>
2025-02-25 14:35:49 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import Listing_categories from "./components/Listing_categories";
 import Listing_specific_page from "./components/Listing_specific_page";
 import Pricing from "./components/Pricing";
 import Listing_page from "./components/Listing_page";
-import PropertyForm from "./components/Propertyform";
+import PropertyForm from "./components/PropertyForm";
 import BlogForm from "./components/BlogForm";
 import ProductList from "./components/ProductList";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import OtpVerification from "./components/OtpVerification";
-import ProtectedRoute from "./components/Protectedroute";
+import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css'
 import BlogList from "./components/BlogList";
 import ContactsList from "./components/ContactsList";
